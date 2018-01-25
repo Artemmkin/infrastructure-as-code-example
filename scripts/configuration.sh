454de76bf15574c1515defb25366b46d0647ada6
Artem Starostenko <artemstarostenko65@gmail.com>
2018-01-25 23:23:42 +0300

    Add scripts for system configuration

---
+++ scripts/configuration.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -xe
+
+echo "  ----- install ruby and bundler -----  "
+apt-get update
+apt-get install -y ruby-full build-essential
+gem install --no-rdoc --no-ri bundler
+
+echo "  ----- install mongodb -----  "
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list
+apt-get update
+apt-get install -y mongodb-org
+
+echo "  ----- start mongodb -----  "
+systemctl start mongod
+systemctl enable mongod
+
+echo "  ----- copy unit file for application -----  "
+wget https://gist.githubusercontent.com/Artemmkin/ce82397cfc69d912df9cd648a8d69bec/raw/7193a36c9661c6b90e7e482d256865f085a853f2/raddit.service
+mv raddit.service /etc/systemd/system/raddit.service
---
+++ scripts/deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -xe
+
+echo "  ----- clone application repository -----  "
+git clone https://github.com/Artemmkin/raddit.git
+
+echo "  ----- install dependent gems -----  "
+cd ./raddit
+bundle install
+
+echo "  ----- start the application -----  "
+sudo systemctl start raddit
+sudo systemctl enable raddit
