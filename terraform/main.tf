184e88a6f8fb79f5382945670de16a9088d10db6
Artem Starostenko <artemstarostenko65@gmail.com>
2018-01-28 20:31:48 +0300

    Refactor terraform configs

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,19 +1,3 @@
-resource "google_compute_project_metadata" "raddit" {
-  metadata {
-    sshKeys = "raddit-user:${file("~/.ssh/raddit-user.pub")}" // path to ssh key file
-  }
-}
-
-resource "google_compute_firewall" "raddit" {
-  name    = "allow-raddit-tcp-9292"
-  network = "default"
-  allow {
-    protocol = "tcp"
-    ports    = ["9292"]
-  }
-  source_ranges = ["0.0.0.0/0"]
-}
-
 resource "google_compute_instance" "raddit" {
   name         = "raddit-instance"
   machine_type = "n1-standard-1"
@@ -31,4 +15,20 @@ resource "google_compute_instance" "raddit" {
     network = "default"
     access_config {} // use ephemaral public IP
   }
+}
+
+resource "google_compute_project_metadata" "raddit" {
+  metadata {
+    ssh-keys = "raddit-user:${file("~/.ssh/raddit-user.pub")}" // path to ssh key file
+  }
+}
+
+resource "google_compute_firewall" "raddit" {
+  name    = "allow-raddit-tcp-9292"
+  network = "default"
+  allow {
+    protocol = "tcp"
+    ports    = ["9292"]
+  }
+  source_ranges = ["0.0.0.0/0"]
 }
